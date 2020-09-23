d933dd4a8923210851500bac01e4514539ed8a9c
gayoungyeom <gayoungyeom@gmail.com>
2020-09-23 16:08:02 +0900

    [Add] CRUD - Create

--- nodejs/nodejs_opentutorials/main.js
+++ nodejs/nodejs_opentutorials/main.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+const qs = require("querystring");
 
 templateHTML = (title, list, body) => {
   return `
@@ -13,6 +14,7 @@ templateHTML = (title, list, body) => {
     <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
+        <a href="/create">create</a>
         ${body}
     </body>
     </html>
@@ -37,7 +39,7 @@ const app = http.createServer(function (request, response) {
   let title = queryData.id;
 
   if (pathname === "/") {
-    if (queryData.id == undefined) {
+    if (queryData.id == undefined) { //home page
       fs.readdir("./data", (err, filelist) => {
         title = "Welcome";
         description = "Hello, Node.js";
@@ -50,7 +52,7 @@ const app = http.createServer(function (request, response) {
         response.writeHead(200);
         response.end(template);
       });
-    } else {
+    } else { //data page
       fs.readdir("./data", (err, filelist) => {
         fs.readFile(`data/${queryData.id}`, "utf8", (err, description) => {
           const list = templateList(filelist);
@@ -64,6 +66,41 @@ const app = http.createServer(function (request, response) {
         });
       });
     }
+  } else if (pathname === "/create") { //create from page
+    fs.readdir("./data", (err, filelist) => {
+      title = "WEB - Create";
+      const list = templateList(filelist);
+      const template = templateHTML(
+        title,
+        list,
+        `
+        <form action="http://localhost:3000/create_process" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p><textarea name="description" placeholder="description"></textarea></p>
+          <p><input type="submit"></p>
+        </form>
+        `
+      );
+      response.writeHead(200);
+      response.end(template);
+    });
+  } else if (pathname === "/create_process") { //해당 data page
+    let body = '';
+    request.on('data', (data) => {
+      body += data;
+    });
+    request.on('end', () => {
+      let post = qs.parse(body);
+      const title = post.title;
+      const description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
+        // redirect to data page
+        response.writeHead(302, {
+          Location: `/?id=${title}`
+        });
+        response.end('success');
+      })
+    });
   } else {
     response.writeHead(404);
     response.end("Not Found");
